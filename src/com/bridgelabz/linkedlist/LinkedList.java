821de44395b49d3b036730322252889fd458fd62
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-26 01:51:48 +0530

    Added: Ability to add position for Element

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.linkedlist;
 
+import java.util.Scanner;
+
 public class LinkedList {
 
 	Node head; // head of list
@@ -41,6 +43,24 @@ public class LinkedList {
 		}
 	}
 
+	// Add position
+	public void addPosition(Object data, int position) {
+		int index = 0;
+		Node newNode = new Node(data);
+		Node left = head;
+		Node right = left.next;
+
+		while (index < (position - 1)) {
+			left = left.next;
+			right = right.next;
+			index++;
+
+		}
+		newNode.next = right;
+		left.next = newNode;
+
+	}
+
 	/*
 	 * Display position No insertion will take place if list empty
 	 */
@@ -60,20 +80,16 @@ public class LinkedList {
 
 		LinkedList list = new LinkedList();
 
+		System.out.println("Welcome to Linked List Problem");
+		Scanner scanner = new Scanner(System.in); // create an object
 		System.out.println("Adding Nodes At First");
 		list.addFirst(70);
 		list.display();
-		list.addFirst(30);
-		list.display();
 		list.addFirst(56);
 		list.display();
-		System.out.println();
-		System.out.println("Adding Nodes At last");
-		list.addLast(56);
-		list.display();
-		list.addLast(30);
-		list.display();
-		list.addLast(70);
+		System.out.println("Enter Position Number To Add:- ");
+		int position = scanner.nextInt();
+		list.addPosition(30, position);
 		list.display();
 
 	}

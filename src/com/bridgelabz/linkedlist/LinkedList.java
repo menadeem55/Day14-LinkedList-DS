53244dad18fcc92c62fa5bce0ce6b766a6ccac62
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-26 01:40:39 +0530

    Added: Ability to add in last

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -4,25 +4,19 @@ public class LinkedList {
 
 	Node head; // head of list
 
-	/*
-	 * LinkedList Node
-	 */
-
+	/* Linked list Node */
 	class Node {
 		Object data;
 		Node next; // reference to next object in the sequence
 
 		Node(Object data) {
-
 			this.data = data;
 		}
-
 	}
 
 	/*
-	 * create list method create new node to be added
+	 * ADD - FIRST POSITION create new node to be added
 	 */
-
 	public void addFirst(Object data) {
 		Node newNode = new Node(data); // new node with given data
 		newNode.next = head;
@@ -30,9 +24,26 @@ public class LinkedList {
 	}
 
 	/*
-	 * Display position No nsertion will take place if list is empty
+	 * ADD - LAST POSITION if the linked list empty. then make the new node as head
 	 */
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+		Node temp = head;
+		if (head == null) {
+			head = newNode;
+		}
 
+		else {
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+	}
+
+	/*
+	 * Display position No insertion will take place if list empty
+	 */
 	public void display() {
 		Node temp = head;
 		while (temp != null) {
@@ -40,19 +51,29 @@ public class LinkedList {
 				System.out.print(temp.data + " -> ");
 			else
 				System.out.println(temp.data);
-			temp = temp.next; // advance to next node in the list
+			temp = temp.next; // Advance to next node in the list
+
 		}
 	}
 
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
-		System.out.println("welcome to LinkedList Problem");
+
+		System.out.println("Adding Nodes At First");
 		list.addFirst(70);
 		list.display();
 		list.addFirst(30);
 		list.display();
 		list.addFirst(56);
+		list.display();
+		System.out.println();
+		System.out.println("Adding Nodes At last");
+		list.addLast(56);
+		list.display();
+		list.addLast(30);
+		list.display();
+		list.addLast(70);
 		list.display();
 
 	}

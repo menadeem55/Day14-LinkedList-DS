8d44f76650f91fde9281b926e418e55d4413cc7f
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-25 15:05:34 +0530

    Added: Created simple linked list

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -2,9 +2,41 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedList {
 
+	Node head; // head of list
+
+	/*
+	 * LinkedList Node
+	 */
+
+	class Node {
+		Object data;
+		Node next; // reference to next object in the sequence
+
+		Node(Object data) {
+
+			this.data = data;
+		}
+
+	}
+
+	/*
+	 * Create list method create new node to be added
+	 */
+
+	public void CreateList(Object data) {
+
+		Node newNode = new Node(data); // new node with given data
+		System.out.println(newNode.data + " "); // print data at current node
+	}
+
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Linked List Problem");
+		LinkedList list = new LinkedList();
+		System.out.println("welcome to LinkedList Problem");
+		list.CreateList(56);
+		list.CreateList(30);
+		list.CreateList(70);
+
 	}
 
 }
